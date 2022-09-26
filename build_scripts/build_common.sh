9feb4b7e1270ea2191ca6c906b1069be05c51e3d
Yazan Shhady <yazan.shhady@solid-run.com>
2022-09-26 23:10:45 +0300

    first commit

---
+++ build_scripts/build_common.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+#########################################################
+# This file contains functions common to all the scripts
+# Each script sour 'source' this file at the beginning.
+#########################################################
+
+
+
+# Toolchain Selection GUI
+# Since each sub-script will want to ask the user what toolchain to use, we will keep a common interface in this file.
+# $1 = env variable to save TOOLCHAIN_SETUP_NAME
+# $2 = env variable to save TOOLCHAIN_SETUP
+select_toolchain() {
+
+    SELECT=$(whiptail --title "Toolchain setup" --menu "You may use ESC+ESC to cancel.\nEnter the command line you want to run before build.\n" 0 0 0 \
+	"1  ARM gcc-arm-11.2-2022.02" "  /opt/arm/gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf" \
+	"0  (none)" "  default -> ROOTDIR/build/toolchain/gcc-arm*" \
+	3>&1 1>&2 2>&3)
+  RET=$?
+  if [ $RET -eq 0 ] ; then
+    case "$SELECT" in
+      1\ *)
+      		x_TOOLCHAIN_SETUP_NAME="ARM gcc-arm-10.3-2021.1"
+      		x_TOOLCHAIN_SETUP="PATH=/opt/arm/gcc-arm-none-eabi-10.3-2021.1/bin:\$PATH ; export CROSS_COMPILE=aarch64-none-elf-" ;;
+      0\ *)
+		x_TOOLCHAIN_SETUP_NAME="(none)"
+		x_TOOLCHAIN_SETUP= ;;
+      *) whiptail --msgbox "Programmer error: unrecognized option" 20 60 1 ;;
+    esac || whiptail --msgbox "There was an error running option $SELECT" 20 60 1
+  fi
+
+  DO_SET="export $1=\"$x_TOOLCHAIN_SETUP_NAME\""
+  eval "$DO_SET"
+
+  DO_SET="export $2=\"$x_TOOLCHAIN_SETUP\""
+  DO_SET=$(echo "$DO_SET" | sed s/\$PATH/\\\\\$PATH/)  # Keep $PATH from being expanded
+  eval "$DO_SET"
+}
+
+
+read_setting() {
+  if [ -e "$SETTINGS_FILE" ] ; then
+    source "$SETTINGS_FILE"
+  else
+    echo -e "\nERROR: Settings file ($SETTINGS_FILE) not found."
+    exit
+  fi
+}
+
+# $1 = env variable to save
+# $2 = value
+# Remember, we we share this file with other scripts, so we only want to change
+# the lines used by this script
+save_setting() {
+
+
+  if [ ! -e $SETTINGS_FILE ] ; then
+    touch $SETTINGS_FILE # create file if does not exit
+  fi
+
+  # Do not change the file if we did not make any changes
+  grep -q "^$1=$2$" $SETTINGS_FILE
+  if [ "$?" == "0" ] ; then
+    return
+  fi
+
+  sed '/^'"$1"'=/d' -i $SETTINGS_FILE
+  echo  "$1=$2" >> $SETTINGS_FILE
+
+  # Delete empty or blank lines
+  sed '/^$/d' -i $SETTINGS_FILE
+
+  # Sort the file to keep the same order
+  sort -o $SETTINGS_FILE $SETTINGS_FILE
+}
+
+# Check for required Host packages
+# If a package is missing, then kill the script (exit)
+check_packages() {
+
+  MISSING_A_PACKAGE=0
+  PACKAGE_LIST=(git make gcc g++ python3 bison flex)
+
+  for i in ${PACKAGE_LIST[@]} ; do
+    CHECK=$(which $i)
+    if [ "$CHECK" == "" ] ; then
+      echo "ERROR: Missing host package: $i"
+      MISSING_A_PACKAGE=1
+    fi
+  done
+  CHECK=$(dpkg -l 'libncurses5-dev' | grep '^ii')
+  if [ "$CHECK" == "" ] ; then
+    MISSING_A_PACKAGE=1
+  fi
+
+  # File /usr/include/openssl/sha.h is required to build Trusted Firmware-A
+  CHECK=$(dpkg -l 'libssl-dev' | grep '^ii')
+  if [ "$CHECK" == "" ] ; then
+    MISSING_A_PACKAGE=1
+  fi
+
+  if [ "$MISSING_A_PACKAGE" != "0" ] ; then
+    echo "ERROR: Missing mandatory host packages"
+    echo "Please make sure the following packages are installed on your machine."
+    echo "    ${PACKAGE_LIST[@]} libncurses5-dev libncursesw5-dev libssl-dev"
+    echo ""
+    echo "The following command line will ensure all packages are installed."
+    echo ""
+    echo "   sudo apt-get install ${PACKAGE_LIST[@]} libncurses5-dev libncursesw5-dev"
+    echo ""
+    exit 1
+  fi
+}
---
+++ runme.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+set -e
+
+###############################################################################
+# General configurations
+###############################################################################
+BUILDROOT_VERSION=2020.02.1
+
+mkdir -p build images
+ROOTDIR=`pwd`
+PARALLEL=$(getconf _NPROCESSORS_ONLN) # Amount of parallel jobs for the builds
+TOOLS="tar git make 7z dd mkfs.ext4 parted mkdosfs mcopy dtc iasl mkimage e2cp truncate qemu-system-aarch64 cpio rsync bc bison flex python unzip pandoc meson ninja depmod"
+TOOLS_PACKAGES="build-essential git dosfstools e2fsprogs parted sudo mtools p7zip p7zip-full device-tree-compiler acpica-tools u-boot-tools e2tools qemu-system-arm libssl-dev cpio rsync bc bison flex python unzip pandoc meson ninja-build kmod"
+
+export PATH=$ROOTDIR/build/toolchain/gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf/bin:$PATH
+export CROSS_COMPILE=aarch64-none-elf-
+export ARCH=arm64
+
+echo "Checking all required tools are installed"
+
+set +e
+for i in $TOOLS; do
+	TOOL_PATH=`which $i`
+	if [ "x$TOOL_PATH" == "x" ]; then
+		echo "Tool $i is not installed"
+		echo "If running under apt based package management you can run -"
+		echo "sudo apt install $TOOLS_PACKAGES"
+		exit -1
+	fi
+done
+set -e
+
+# Check if git is configured
+GIT_CONF=`git config user.name || true`
+if [ "x$GIT_CONF" == "x" ]; then
+	echo "git is not configured! using fake email and username ..."
+	export GIT_AUTHOR_NAME="SolidRun rzg2l_build Script"
+	export GIT_AUTHOR_EMAIL="support@solid-run.com"
+	export GIT_COMMITTER_NAME="${GIT_AUTHOR_NAME}"
+	export GIT_COMMITTER_EMAIL="${GIT_AUTHOR_EMAIL}"
+fi
+
+if [[ ! -d $ROOTDIR/build/toolchain ]]; then
+	mkdir -p $ROOTDIR/build/toolchain
+	cd $ROOTDIR/build/toolchain
+	wget https://developer.arm.com/-/media/Files/downloads/gnu/11.2-2022.02/binrel/gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf.tar.xz
+	tar -xvf gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf.tar.xz
+fi
+
+cd $ROOTDIR
+###############################################################################
+# Source code clonig
+###############################################################################
+BOOT_LOADER_DDIR='./sd_boot'
+if ! [[ -d build/$BOOT_LOADER_DDIR ]]; then
+  echo "Source code clonig..."
+  # ================ Clone Sources =========== #
+
+  # ================ Clone ATF =========== #
+  mkdir -p build/$BOOT_LOADER_DDIR
+  cd build/$BOOT_LOADER_DDIR
+  git clone -b v2.6/rz https://github.com/renesas-rz/rzg_trusted-firmware-a
+  cd rzg_trusted-firmware-a
+  wget https://raw.githubusercontent.com/seebe/rzg_stuff/master/tfa_patches/0001-SD-boot-support.patch
+  patch -p1 < 0001-SD-boot-support.patch
+  cd ..
+
+  # ================ Clone U-Boot =========== #
+  git clone -b v2021.10/rz https://github.com/renesas-rz/renesas-u-boot-cip
+
+  # ================ Install help scripts ====== #
+  cp $ROOTDIR/build_scripts/*.sh $ROOTDIR/build/$BOOT_LOADER_DDIR
+  chmod +x $ROOTDIR/build/${BOOT_LOADER_DDIR}/*.sh
+
+fi
+
+### For debug
+cp $ROOTDIR/build_scripts/*.sh $ROOTDIR/build/$BOOT_LOADER_DDIR
+chmod +x $ROOTDIR/build/${BOOT_LOADER_DDIR}/*.sh
+####
+
+###############################################################################
+# Building boot loader
+###############################################################################
+echo "Building boot loader..."
+cd $ROOTDIR/build/$BOOT_LOADER_DDIR
+# Select toolchain that you have:
+./build.sh s
+# build u-boot:
+./build.sh u
+# build ATF
+./build.sh t
